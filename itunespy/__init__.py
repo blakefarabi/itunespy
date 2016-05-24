1e1dac602104ec64d4203e7cc4e8327e5d35c535
blakefarabi <blake@Blakes-Mac-Pro.local>
2016-05-24 17:33:10 -0500

    requests

--- itunespy/__init__.py
+++ itunespy/__init__.py
@@ -13,7 +13,7 @@
 #  copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>
 
-import requests
+from requests import *
 from itunespy import music_artist
 from itunespy import music_album
 from itunespy import movie_artist
